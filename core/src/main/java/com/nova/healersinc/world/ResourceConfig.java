5acb727454a81e0319fff9c86b09c11334fb734a
Su5upern0va <monarch38108@gmail.com>
2025-12-15 14:23:33 +0100

    refactor(world): replace `ResourceDefinition` with streamlined `ResourceConfig` for resource metadata management

---
+++ core/src/main/java/com/nova/healersinc/world/ResourceConfig.java
@@ -0,0 +1,26 @@
+package com.nova.healersinc.world;
+
+import java.util.List;
+
+public class ResourceConfig {
+    public List<ResourceEntry> resources;
+
+    public static class ResourceEntry {
+        public String id;
+        public String name;
+        public String category;
+        public Visual visual;
+        public Properties properties;
+    }
+
+    public static class Properties {
+        public int stackSize;
+        public int baseValue;
+    }
+
+    public static class Visual {
+        public String color;
+        public String texture;
+        public String icon;
+    }
+}
